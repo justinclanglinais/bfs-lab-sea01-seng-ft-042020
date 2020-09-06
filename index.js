ed76002bffe3aeb41d0abfde2710322c85062019
Justin Langlinais <justinclanglinais@gmail.com>
2020-09-06 14:46:20 -0700

    solving

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 function bfs(rootNode, vertices, edges){
-
+    rootNode.distance = 0
+    queue = [rootNode]
+    while (queue != 0) {
+        let currentNode = queue.shift()
+        let adjacentNodes = findAdjacent(currentNode.name, edges, vertices)
+        for vertex in adjacentVeritices {
+            markDistanceAndPredecessor(vertex)
+            addToQueue(vertex)
+        }
+    }
+}
+function findAdjacent (nodeName, edges, vertices) {
+    let adjacent = []
+    for edge in edges {
+        if edge.includes?(nodeName) {
+            adjacent.push(edge)
+        }
+    }
+    return adjacent
 }
